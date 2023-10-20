9f561eabb19aa5295b9ecc4ea7af94f9dd67dde2
okurusou <okurusou@gmail.com>
2023-10-20 20:41:53 +0900

    import문

---
+++ study/java/seven/Ex7_3.java
@@ -0,0 +1,15 @@
+package seven;
+
+import test.Test12;
+
+public class Ex7_3 {
+
+	public static void main(String[] args) {
+		
+		
+//		test.Test12 t = new test.Test12(); // test패키지 내의 Test12클래스 객체 생성
+		
+		// 위의 방법 또한 가능하지만 import문을 사용한 방식이 훨씬 가독성과 코드작성의 편리성에 있어서 유리함
+		Test12 t = new Test12();
+	}
+}
